897ee5b1dc02108b49cce8ae1ff9a5f9743e027e
mervinkogei <vincent.kogei@yahoo.com>
2019-11-22 14:27:55 +0300

    setup database to insert objects to the database

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -12,4 +12,14 @@ public class Animals {
     public String getName(){
         return name;
     }
+    @Override
+    public boolean equals(Object otherAnimals){
+        if (!(otherAnimals instanceof Animals)) {
+            return false;
+        } else {
+            Animals newAnimals = (Animals) otherAnimals;
+            return this.getName().equals(newAnimals.getName()) &&
+                    this.getName().equals(newAnimals.getName());
+        }
+    }
 }
--- src/test/java/AnimalsTest.java
+++ src/test/java/AnimalsTest.java
@@ -1,11 +1,33 @@
 import org.junit.Test;
+import org.junit.*;
+import org.sql2o.*;
+import static org.junit.Assert.*;
 
-import static org.junit.Assert.assertEquals;
+
+//import static junit.framework.TestCase.assertTrue;
+//import static org.junit.Assert.assertEquals;
 
 public class AnimalsTest {
     @Test
-    public void getName_personInstantiatesWithName_Henry() {
-        Animals testAnimals = new Animals(1, "Elphant");
-        assertEquals("1", testAnimals.getName());
+    public void getName_AnimalsInstantiatesWithName_Elephant() {
+        Animals testAnimals = new Animals(1, "Elephant");
+        assertEquals("Elephant", testAnimals.getName());
     }
+    @Test
+    public void getId_AnimalsInstantiatesWithId_Id() {
+        Animals testAnimals = new Animals(1, "Elephant");
+        assertEquals(1, testAnimals.getId());
+    }
+//    @Test
+//    public void equals_returnsTrueIfAnimalsAndEndangeredAnimalAreSame_true() {
+//        Animals sightAnimals = new Animals(1, "Elephant");
+//        Animals EndangeredAnimals = new Animals(1, "Elephants");
+//        assertTrue(sightAnimals.equals(EndangeredAnimals));
+//    }
+@Test
+public void save_insertsObjectIntoDatabase_Animals() {
+    Animals testAnimals = new Animals(1, "Elephant");
+    testAnimals.save();
+    assertTrue(Animals.all().get(0).equals(testAnimals));
+}
 }
