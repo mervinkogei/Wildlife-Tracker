07f9e6f07bf2017dd6e743d5e3a8e7ffd498adfc
mervinkogei <vincent.kogei@yahoo.com>
2019-11-23 11:48:32 +0300

    Create a sighting class and create a constructor for id,location and RangerName

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -1,2 +1,18 @@
 public class Sightings {
+    private int id;
+    private String location;
+    private String rangerName;
+
+    public Sightings (String location, String rangerName){
+        this.id = id;
+        this.location = location;
+        this.rangerName = rangerName;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+    public String getRangerName(){
+        return rangerName;
+    }
 }
