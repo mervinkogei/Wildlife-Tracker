97a1239b33fcde473518f235a2f85d6f9a8d0e9e
mervinkogei <vincent.kogei@yahoo.com>
2019-11-25 07:07:11 +0300

    Create templates directory and add templates animals-form,animals,layout,index,sighting and sighting-form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,11 @@
-import java.util.Map;
-import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
-import static spark.Spark.*;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
 
 
 public class App {
